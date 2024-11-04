3fa70c4f69a51c3efedffed59d2f81f1883009d7
Sunny8085 <sunny98355@outlook.com>
2024-11-04 08:40:46 +0530

    completed

--- src/main/java/com/service/task/controller/InvoiceController.java
+++ src/main/java/com/service/task/controller/InvoiceController.java
@@ -1,6 +1,8 @@
 package com.service.task.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,9 @@ public class InvoiceController {
 	@Autowired private InvoiceService taskSerice;
 	
 	@PostMapping("generate-bill")
-	public String genrateInvoice(@RequestBody InvoiceRequest invoiceRequest){
-		return taskSerice.genrateInvoice(invoiceRequest);
+	public ResponseEntity<String> genrateInvoice(@RequestBody InvoiceRequest invoiceRequest){
+		String invoice = taskSerice.genrateInvoice(invoiceRequest);
+		return new ResponseEntity<>(invoice, HttpStatus.OK);
 	}
 	
 }
